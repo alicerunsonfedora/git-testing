a0d83cd77d295d81440c4b5cbf3374479aff3ebd
Quintin <qumos001@goucher.edu>
2022-02-13 13:44:05 -0500

    Fixed a bug where the inputed value was never converted to an int
    This is the only feature of the program as as such is important.

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ def main():
     print("Hello! Please enter a number and I will double it.")
     
     # FIXME: Can we replace this by grabbing args?
-    value = input("What is your number?")
+    value = int(input("What is your number?\n"))
     double = value * 2 # Multiply value by 2
     
     print(f"Your doubled value is {double}!")
